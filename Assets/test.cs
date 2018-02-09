7b999ecef97a5ff91553c8345336e37ad7d6769e
H-Destiny <2026652784@qq.com>
2018-02-09 11:45:13 +0800

    init

---
+++ Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public GameObject sphere;
+    public Material red;
+    public Material green;
+
+    [ContextMenu("red")]
+    public void changeRed()
+    {
+        sphere.GetComponent<Renderer>().material = red;
+    }
+
+    [ContextMenu("green")]
+    public void changeGreen()
+    {
+        sphere.GetComponent<Renderer>().material = green;
+    }
+}
